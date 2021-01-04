b25a8945e7a6cc32f2968e94a4458ce1674bb7d6
Adheli Tavares <adheli.tavares@gmail.com>
2021-01-04 19:16:58 +0000

    Fix of unit tests

--- src/test/java/ie/ait/tavares/pogo/PogoStatsApplicationTests.java
+++ src/test/java/ie/ait/tavares/pogo/PogoStatsApplicationTests.java
@@ -2,8 +2,10 @@ package ie.ait.tavares.pogo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PogoStatsApplicationTests {
 
 	@Test
